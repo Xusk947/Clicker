2d6d676101eb2d8f9ff7d8c5cf27b161a042b040
Xusk <Xusk947@gmail.com>
2020-07-10 06:09:35 +0500

    some fix

--- main.lua
+++ main.lua
@@ -1,5 +1,10 @@
 display.setDefault("magTextureFilter","nearest")
 
+background = display.newImageRect( "assets/background.png", display.viewableContentWidth * 2,display.viewableContentHeight * 2.2)
+background.x = 0
+background.y = 0
+
+
 coin = display.newImageRect( "assets/coin.png", 100,100)
 coin.x = display.contentCenterX
 coin.y = display.contentCenterY
@@ -16,13 +21,15 @@ upgrade = display.newImageRect( "assets/upgrade.png",200,50 )
 upgrade.x = display.contentCenterX 
 upgrade.y = display.contentCenterY * 2
 function tap( event )
-	if event.phase == "begin" or  event.phase == "ended" or event.phase == "moved" then 
+	if event.phase == "begin" or  event.phase == "ended"  then 
 		coins = coins + tapBonus
 		coinsText.text = coins
-		--coin.width = coin.width + 10
-		--coin.height = coin.height + 10
-		coin.heat = coin.heat + 0.01
-		if coin.heat < 0.1 then 
+		coin.width = coin.width + 10
+		coin.height = coin.height + 10
+		if coin.heat <= 1 then 
+			coin.heat = coin.heat + 0.01
+		end
+		if coin.heat < 0.5 then 
 			coin:setFillColor( 1,1,1,1)
 		else
 			coin:setFillColor( coin.heat, 0.5,0.5, 1 )
@@ -34,11 +41,11 @@ function tap( event )
 end
 
 function upgradee( event )
-	if event.phase == "begin" or event.phase == "ended" or event.phase == "moved" and coins >= upgradeCost then
+	if (event.phase == "begin" or event.phase == "ended" ) and coins >= upgradeCost then
 		tapBonus = tapBonus + 1
 		coins = coins - upgradeCost
 		coinsText.text = coins
-		upgradeCost = math.floor( upgradeCost * 1.5 )
+		upgradeCost = math.floor( upgradeCost * 1.35 )
 		cost.text = upgradeCost
 		upgrade.width = upgrade.width + 10
 		upgrade.height = upgrade.height + 10
@@ -49,7 +56,9 @@ function upgradee( event )
 end
 
 function update()
-	coin.heat = coin.heat - 0.01
+	if coin.heat > 0 then
+		coin.heat = coin.heat - 0.001
+	end
 	if coin.heat < 0.3 then 
 		coin:setFillColor( 1,1,1,1)
 	else
@@ -57,6 +66,8 @@ function update()
 	end
 end
 
-timer.performWithDelay( 1,update() )
+
+
+timer.performWithDelay( 10,update , 0)
 coin:addEventListener( "touch", tap )
 upgrade:addEventListener( "touch", upgradee )
